41983d376a08f731c07879ed7ec18963d607ff08
Josip Dudas <josdudas@gmail.com>
2019-06-06 16:51:10 +0200

    added login and signup method

---
+++ User/signup.php
@@ -0,0 +1,46 @@
+<?php
+     
+     // get database connection
+     include_once '../config/database.php';
+      
+     // instantiate user object
+     include_once '../objects/user.php';
+      
+     $database = new Database();
+     $db = $database->getConnection();
+      
+     $user = new User($db);
+      
+     // set user property values
+     $user->id = $_POST['id'];
+     $user->firstname = $_POST['firstname'];
+     $user->lastname = $_POST['lastname'];
+     $user->password = $_POST['password'];
+     $user->email = $_POST['email'];
+     $user->position = $_POST['position'];
+     $user->address = $_POST['address'];
+     $user->sex = $_POST['sex'];
+      
+     // create the user
+     if($user->signup()){
+         $user_arr=array(
+             "status" => true,
+             "message" => "Successfully Signup!",
+             "id" => $user->id,
+            "firstname" => $user->firstname,
+            "ilastnamed" => $user->lastname,
+            "password" => $user->password,
+            "email" => $user->email,
+            "position" => $user->position,
+            "address" => $user->address,
+            "sex" => $user->sex
+         );
+     }
+     else{
+         $user_arr=array(
+             "status" => false,
+             "message" => "Email already exists!"
+         );
+     }
+     print_r(json_encode($user_arr));
+     ?>
